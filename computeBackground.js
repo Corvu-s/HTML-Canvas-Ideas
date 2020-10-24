07b6cc3fbda10df1bcf0aa0f9f48b7cabe41be2a
Corvu-s <roomtemp72@gmail.com>
2020-10-23 20:45:52 -0400

    added a slider

--- computeBackground.js
+++ computeBackground.js
@@ -1,33 +1,40 @@
 var canvas = document.querySelector("canvas");
-
+var slider =document.getElementById("myRange")
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var c = canvas.getContext("2d"); // context, alows us to draw things
 //implemrnt some sort of slider to change the recgangle size
 //this it in another computer
+
+slider.oninput=function(){
+    init()
+}
 var colours = ["#d0ffb5","#c1ff9e","#90ff52","#86ff42","#74ff26","#6aff17","#5bff00"]
 function Rectangle(x,y){
     min=0;
     max=255;
     this.dir=1;
-    this.default=Math.floor(Math.random()* (max -min)+min)
-    this.ci1=this.default
+    this.red=Math.floor(Math.random()* (max -min)+min)
     this.Xpos=x;
     this.Ypos=y;
     this.draw =function(){
-    c.fillStyle = `rgba(${this.ci1},${this.ci1},${this.ci1})`;
-    c.fillRect(x*100, y*100,100, 100);
+    c.fillStyle = `rgba(${this.red},${this.red},${this.red})`;
+    c.fillRect(x*slider.value, y*slider.value,slider.value, slider.value);
     
     }
     this.update= function(){
         
-         var speed =Math.floor((Math.random()*5) +1)
-        if(this.ci1 > 225){// if you hit the top start 
+        // var speed =Math.floor((Math.random()*5) +1)
+         var speed =2
+
+        if(this.red > 225){// if you hit the top start 
             this.dir= -speed;
-        }else if( this.ci1 <40){
+        }else if( this.red <40){
             this.dir =speed;
         }
-        this.ci1 =this.ci1 + this.dir
+        this.red =this.red + this.dir
+     
+ 
         this.draw();
     }
 }
@@ -35,8 +42,9 @@ function Rectangle(x,y){
 var rectArray=[];
 function init(){
     //rectArray.push(new Rectangle(0,0))
-    for( i=0;i<Math.floor((window.innerWidth)/100);i++){
-        for(j=0;j<Math.floor((window.innerHeight)/100);j++){
+    rectArray=[]
+    for( i=0;i<Math.floor((window.innerWidth)/slider.value);i++){
+        for(j=0;j<Math.floor((window.innerHeight)/slider.value);j++){
             rectArray.push(new Rectangle(i,j))
 
         }
@@ -45,9 +53,10 @@ function init(){
 
 
 
-setInterval(animate,100)
+setInterval(animate,60)
 
 function animate() {
+    console.log(slider.value)
     //requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
   
