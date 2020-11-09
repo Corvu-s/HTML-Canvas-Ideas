ad7501125b7a0d4e48053248bf218e18c5e394f5
Linigari-Luke-216307639 <46363148+Linigari-Luke-216307639@users.noreply.github.com>
2020-11-09 12:58:53 -0500

    mouse things for the rectangle pulsating thing

--- Pulsating squares/pulsatingSquare.js
+++ Pulsating squares/pulsatingSquare.js
@@ -4,8 +4,27 @@ var c=canvas.getContext("2d")
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var colours={black:"black",red:"red"}
-function Square(x,y,id,speed){
+
+var colours=["#A80027","#000000","#363636","#DCDCDC","#FFFAFA"]
+
+var mouse={
+    x:undefined,
+    y:undefined
+}
+
+window.addEventListener("mousemove",(e)=>{
+    mouse.x=e.x;
+    mouse.y=e.y;
+})
+window.addEventListener("mousedown",()=>{
+    clicked=true;
+})
+window.addEventListener("mouseup",()=>{
+    clicked = false;
+})
+
+var clicked=false;
+function Square(x,y,id,speed,col){
     this.id=id
     this.dir=speed;
     this.maxLength=40;
@@ -18,14 +37,9 @@ function Square(x,y,id,speed){
     this.x=x;
     this.y=y;
     this.draw = function(){
-        var col;
-        if(this.id %2 == 0){
-            col=colours.black
-        }else{
-            col=colours.red
-        }
+       
         
-        c.fillStyle=col
+        c.fillStyle=colours[col]
         c.fillRect(x,y,this.length,this.width)
     }
     this.update = function(){
@@ -41,19 +55,28 @@ function Square(x,y,id,speed){
         }
         this.length=this.length+this.dir;
         this.width=this.width+this.dir;
-     
+        
+        if(clicked&&mouse.x>this.x && mouse.x<this.x+40 && mouse.y>this.y && mouse.y>this.y+40){
+            console.log(this.id)
+        }
+
         this.draw()
     }
 }
 
 var squares=[]
+var runners=[]
 function init(){
 squares=[]
+runners=[]
 var id=0;
-for(i=0;i<innerWidth;i=i+80){
-    for(j=0;j<innerHeight;j=j+80){
-        var s = Math.random()*0.5;
-        squares.push(new Square (i,j,id,s))
+
+
+for(i=0;i<innerWidth;i=i+40){
+    for(j=0;j<innerHeight;j=j+40){
+        var s = Math.random()*0.2;
+        var colChoice=Math.floor(Math.random()*5)
+        squares.push(new Square (i,j,id,s,colChoice))
         id++;
     }
 }
@@ -65,7 +88,9 @@ c.clearRect(0, 0, innerWidth, innerHeight);
 
 for(k=0;k<squares.length;k++){
     squares[k].update()
+    
 }
+
 }
 init()
 animate()
